51292df540a25936b14958cd421b2d2e451e4d58
leonel-cloud-ops <leonelacevedo08@gmail.com>
2026-04-11 23:03:35 -0600

    Parcial 2 Fundamentos de programacion, Ejercio 2 declarar una variable con cadena elefante utilizar metodo correspondiente para eliminar los espacios en blanco en ambos estremos de la palabra y mostrar la cantidad de veces que se repite la letra e

---
+++ Ejercicio1P2.py
@@ -0,0 +1,19 @@
+# Ejercicio 1
+# 1. Declara una variable con la cadena "  elefante  ".
+# 2. Utiliza el método correspondiente para eliminar los espacios en blanco a ambos extremos de la palabra.
+# 3. Cuenta y muestra cuántas veces se repite la letra "e" en el texto ya limpio.
+
+nombre = "elefnte"
+print(nombre)
+Verificar_espacios = "  elefante  "
+textoSinEspacios = "verificar_espacios.strip()"
+print("verificar_espacios")
+print(textoSinEspacios)
+
+nuevoTexto = "Inicio con una e"
+inicial = nuevoTexto.startswith("e")
+print(inicial)
+
+finalTexto = "Este texto termina con una e"
+final = finalTexto.endswith("e")
+print
