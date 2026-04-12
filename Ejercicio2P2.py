a29432fc55e412e96bec34d5168b9621c1a9ca43
leonel-cloud-ops <leonelacevedo08@gmail.com>
2026-04-11 23:08:08 -0600

    Parcial 2 Fundamentos de programacion Ejercicio 2 tomar la cadena de texto de su nombre comvertir el texto para que cada primera letra de cada palabra este en mayusculas

---
+++ Ejercicio2P2.py
@@ -0,0 +1,8 @@
+# Ejercicio 2
+# 1. Toma la cadena de texto "Su nombre"".
+# 2. Convierte el texto para que la primera letra de cada una de las palabras este en mayúscula.
+texto = "su nombre"
+print("primera letra de cada una de las palabras en mayusculas")
+nombre = "jose leonel acevedo villacorto"
+nombre_primera_letra_mayusculas = nombre.title()
+resultado = "texto_cap".replace("Su Nombre", "Su Apellido")
