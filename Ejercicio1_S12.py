a41ee810e93f3dc81cbb0162f55007acf3a33d08
leonel-cloud-ops <leonelacevedo08@gmail.com>
2026-04-18 19:05:07 -0600

    Ejercicio 1 de la semana 12

---
+++ Ejercicio1_S12.py
@@ -0,0 +1,8 @@
+numero = float(input("Ingresa un numero: "))
+
+if numero > 0:
+    print("El numero es positivo.")
+elif numero < 0:
+    print("El numero es negativo.")
+else:
+    print("El numero es cero.")
